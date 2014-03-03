f939350e484bbb5696a9f4d17e2be86cb3f664f7
Greg Payne <greg.payne@gmail.com>
2014-03-03 14:50:31 -0600

    Download multiple emails in one pass.

--- src/Console/CommandDownload.php
+++ src/Console/CommandDownload.php
@@ -38,16 +38,14 @@ class CommandDownload extends \Symfony\Component\Console\Command\Command {
         continue;
       }
 
-      // Initialize the mbox.
-      if ($output->isVerbose()) {
-        $output->writeln("Initialize the mbox.");
-      }
-      $mbox_path = tempnam($root, strftime('.email.%Y%m%d%H%M%S.'));
-      touch($mbox_path);
-      chmod($mbox_path, 0777);
-
       // Apply default settings to the configuration.
       $conf = array_replace_recursive(array(
+        'title' => 'Untitled',
+        'link' => 'http://example.com/test/',
+        'description' => 'This is the archive for untitled.',
+        'baseurl' => '/test/',
+        'basepath' => '/tmp/',
+        'max_downloads' => 1,
         'getmail' => array(
           'options' => array(
             'verbose' => 0,
@@ -72,47 +70,62 @@ class CommandDownload extends \Symfony\Component\Console\Command\Command {
           'destination' => array(
             'type' => 'Mboxrd',
             'user' => 'apache',
-            "path" => $mbox_path,
+            "path" => NULL,
           ),
         ),
       ));
 
-      // Build an ini file for getmail.
-      if ($output->isVerbose()) {
-        $output->writeln("Getmail: writing configuration");
-      }
-      $getmail_ini_path = tempnam($root, '.getmailconf.');
-      $data = '';
-      foreach ($conf['getmail'] as $section_id => $params) {
-        $data .= "[$section_id]\n";
-        foreach ($params as $k => $v) {
-          if (is_string($v)) {
-            if ($v === '') {
-              $v = 0;
-            }
-          }
-          elseif (is_bool($v)) {
-            $v = $v ? '1' : '0';
-          }
-          $data .= $k . ' = ' . $v . "\n";
+      $count = 0;
+      while (!$count || $count < $conf['max_downloads']) {
+        // Initialize the mbox.
+        $count++;
+        if ($output->isVerbose()) {
+          $output->writeln("Initialize mbox #$count.");
         }
-      }
-      file_put_contents($getmail_ini_path, $data);
+        $mbox_path = tempnam($root, strftime('.email.%Y%m%d%H%M%S.'));
+        touch($mbox_path);
+        chmod($mbox_path, 0777);
 
-      // Execute getmail
-      if ($output->isVerbose()) {
-        $output->writeln("Getmail: running.");
-      }
-      $getmail_output = shell_exec("getmail -r "
-        . escapeshellarg($getmail_ini_path));
+        // Build an ini file for getmail.
+        $conf['getmail']['destination']['path'] = $mbox_path;
+        if ($output->isVerbose()) {
+          $output->writeln("Getmail: writing configuration");
+        }
+        $getmail_ini_path = tempnam($root, '.getmailconf.');
+        $data = '';
+        foreach ($conf['getmail'] as $section_id => $params) {
+          $data .= "[$section_id]\n";
+          foreach ($params as $k => $v) {
+            if (is_string($v)) {
+              if ($v === '') {
+                $v = 0;
+              }
+            }
+            elseif (is_bool($v)) {
+              $v = $v ? '1' : '0';
+            }
+            $data .= $k . ' = ' . $v . "\n";
+          }
+        }
+        file_put_contents($getmail_ini_path, $data);
 
-      // Getmail cleanup
-      if ($output->isVerbose()) {
-        $output->writeln("Getmail: cleanup.");
-      }
-      unlink($getmail_ini_path);
-      if (filesize($mbox_path) == 0) {
-        unlink($mbox_path);
+        // Execute getmail
+        if ($output->isVerbose()) {
+          $output->writeln("Getmail: running.");
+        }
+        $getmail_output = shell_exec("getmail -r "
+          . escapeshellarg($getmail_ini_path));
+
+        // Getmail cleanup
+        if ($output->isVerbose()) {
+          $output->writeln("Getmail: cleanup.");
+        }
+        unlink($getmail_ini_path);
+        if (filesize($mbox_path) == 0) {
+          unlink($mbox_path);
+          // If nothing was downloaded, then stop.
+          break;
+        }
       }
 
       // Import the file into MHonArc
