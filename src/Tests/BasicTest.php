750e81b5feff86faea79c8554df07accf484ea69
Greg Payne <greg.payne@gmail.com>
2014-06-10 15:23:23 -0500

    Added placeholder for initial unit tests.

---
+++ src/Tests/BasicTest.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the Phonarc package.
+ *
+ * (c) Greg Payne
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Phonarc\Tests;
+
+class BasicTest extends \PHPUnit_Framework_TestCase {
+
+}
